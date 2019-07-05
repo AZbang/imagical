dd1e07d8a49c9dd393256ae355d2df9fc77af25f
AZbang <azbango5@gmail.com>
2019-07-06 02:01:19 +0300

    Updates

---
+++ precache-manifest.8c4dfe8764bc1a7cfdc4c9b53a3043b1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bcc00c700cdabadda43171327a9650cb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c6dcbdf2a13baf71a927",
+    "url": "/static/css/main.f4fc3313.chunk.css"
+  },
+  {
+    "revision": "0c6c3271e47bd74f7949",
+    "url": "/static/js/2.e8f86a45.chunk.js"
+  },
+  {
+    "revision": "c6dcbdf2a13baf71a927",
+    "url": "/static/js/main.92cb04ec.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
